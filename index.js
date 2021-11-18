dcfd925623cb661b95ca2e87a8d5131bf8d3aaf0
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-11-18 15:55:28 +0200

    Add info page, exercise 3.2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const info = `<p>Phonebook has info for ${persons.length} people</p>
+                <p>${new Date()}</p>`
+  res.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
