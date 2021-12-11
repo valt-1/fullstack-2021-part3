a79d4ba27a4322933fa8a48ea3ab85dc00ce4a39
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-12-11 22:15:05 +0200

    Add possibility to delete phonebook entry by id, exercise 3.4

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ app.get('/api/persons/:id', (req, res) => {
   else res.status(404).end()
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
