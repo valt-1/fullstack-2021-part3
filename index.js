117eac2164458e00708e5ba7330fd770a250b8aa
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-12-11 22:51:24 +0200

    Add possibility to post new person, exercise 3.5

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
   },
   {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
   },
   {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
   },
   {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
   }
 ]
 
@@ -45,6 +47,23 @@ app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
   res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  const id = Math.floor(Math.random() * 1000000)
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 const PORT = 3001
