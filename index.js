78bf9534bef43bce3041c3a0a1053d791800a693
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-01-03 13:30:32 +0200

    Edit morgan configs, exercise 3.8

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => {
+  if (req.method === 'POST') return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
