c6a021d09937792e9257aef13b5e4af39191852e
Emma Valtonen <emma.valtonen@helsinki.fi>
2022-01-04 16:48:34 +0200

    Remove deleted contacts from db, exercise 3.15

--- index.js
+++ index.js
@@ -14,29 +14,6 @@ morgan.token('body', (req, res) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
-
 app.get('/info', (req, res) => {
   Person.count({}).then(result => {
     const info = `<p>Phonebook has info for ${result} people</p>
@@ -59,9 +36,9 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+  Person.findByIdAndRemove(req.params.id).then(result => {
+    res.status(204).end()
+  })
 })
 
 
